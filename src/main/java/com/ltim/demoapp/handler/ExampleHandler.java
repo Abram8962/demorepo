d0652756479e333ee4eb78fb26eef6afb85f8dab
raifoddin <rafi14.rsr@gmail.com>
2024-01-23 18:33:02 +0530

    added secret manager support

--- src/main/java/com/ltim/demoapp/handler/ExampleHandler.java
+++ src/main/java/com/ltim/demoapp/handler/ExampleHandler.java
@@ -1,5 +1,6 @@
 package com.ltim.demoapp.handler;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -10,8 +11,19 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class ExampleHandler {
-  public Mono<ServerResponse> hello(ServerRequest request) {
-    return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
-         .body(BodyInserters.fromObject("Hello, Spring WebFlux Example!"));
-  }
+
+	@Value("${dbname}")
+	private String dbname;
+	
+	@Value("${dbusername}")
+	private String dbusername;
+	
+	@Value("${dbpassword}")
+	private String dbpassword;
+
+	public Mono<ServerResponse> hello(ServerRequest request) {
+		String secrets = " { dbname : "+dbname +" || dbusername : "+ dbusername +" || dbpassword : "+dbpassword +" }";
+		return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+				.body(BodyInserters.fromObject("Secret manager values : "+ secrets));
+	}
 }
